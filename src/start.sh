b8cb18aa791017c7c1009fc8d145e53bcb872674
David Fojtík <66263283+davefojtik@users.noreply.github.com>
2023-12-12 16:40:10 +0100

    added outputs clear in start.sh

--- src/start.sh
+++ src/start.sh
@@ -2,6 +2,10 @@
 
 echo "Worker Initiated"
 
+# Comment/Uncomment this part to clear outputs folder on each run
+echo "Clearing outputs"
+rm -rf /workspace/outputs/files/*
+
 echo "Starting WebUI API"
 cd /workspace
 python main.py --sync-repo skip --disable-private-log --skip-pip &
